c78229c25909a1b777e0a9e2bb62d85869d9ab14
willdevps <willgamer642@gmail.com>
2023-09-23 20:17:49 -0300

    Meu perfil

---
+++ resources/views/minha-rota.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <html data-theme="black"></html>
+
+    <title>Document</title>
+
+<!-- Fonts -->
+
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.7.7/dist/full.css" rel="stylesheet" type="text/css" />
+<script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+    <body> 
+    <div class="hero min-h-screen bg-base-200">
+  <div class="hero-content flex-col lg:flex-row"> 
+    <img src="https://www.bing.com/images/blob?bcid=TjjVexZNYxwGxKe1Ip4C1d43Tdjz.....3c" class="max-w-sm rounded-lg shadow-2xl" />
+     <div>
+      <h1 class="text-5xl font-bold">Olá esse é o meu site pessoal</h1>
+      <p class="py-6">Meu nome é William, sou estudante de G.T.I e esse é um dos meus trabalhos.</p>
+      
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
